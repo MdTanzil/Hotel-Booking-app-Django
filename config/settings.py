af0147952b81794cd899a88e7ce7617b8dd743bd
Md Tanzil <mdtanzil.py@gmail.com>
2023-03-02 22:30:19 +0600

    mysql database settting

--- config/settings.py
+++ config/settings.py
@@ -76,11 +76,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'db_hotel_booking',
+    'USER': 'root',
+    'PASSWORD': '',
+    'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+    'PORT': '3306',
+    'OPTIONS': {
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        'charset': 'utf8mb4',
     }
-}
+} }
 
 
 # Password validation
